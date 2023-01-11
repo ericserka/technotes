ca010a3948cec803c54b0153f896f0f4c1de517a
ericserka <ericserka@hotmail.com>
2023-01-11 01:29:43 -0300

    feat/client: login and logout

--- client/src/features/notes/NewNote.jsx
+++ client/src/features/notes/NewNote.jsx
@@ -5,7 +5,7 @@ import { NewNoteForm } from './NewNoteForm'
 export const NewNote = () => {
   const users = useSelector(selectAllUsers)
 
-  const content = users ? <NewNoteForm users={users} /> : <p>Loading...</p>
+  if (!users.length) return <p>Not Currently Available</p>
 
-  return content
+  return <NewNoteForm users={users} />
 }
